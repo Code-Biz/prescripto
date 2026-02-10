2b5fb8bce6488981714454e335c8eccf5f521cab
code-biz <honeybiz@gmail.com>
2026-02-11 00:38:50 +0500

    coded the adminController part where the doctor data is posted to DB

--- backend/controllers/adminController.js
+++ backend/controllers/adminController.js
@@ -1,3 +1,7 @@
+import validator from "validator"
+import bcrypt from 'bcrypt'
+import { v2 as cloudinary } from 'cloudinary'
+import doctorModel from '../models/doctorModel.js'
 
 
 // API for adding doctor
@@ -6,9 +10,45 @@ const addDoctor = async (req, res) => {
     try {
 
         const { name, email, password, speciality, degree, experience, about, fees, address } = req.body;
+        const imageFile = req.file;
+
+        //checking for all to add doctor
+        if (!name || !email || !password || !speciality || !degree || !experience || !about || !fees || !address) {
+
+            return res.json({ success: false, message: "Details missing" });
+        }
+
+        if (!validator.isEmail(email)) {
+
+            return res.json({ success: false, message: "Please enter valid email address!" });
+        }
+
+        if (password.length < 8) {
+            return res.json({ success: false, message: "Password must have 8 characters atleast!" });
+        }
+
+        const saltRounds = await bcrypt.genSalt(10);
+        const hashedPassword = await bcrypt.hash(password, saltRounds);
+
+        // const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: "image" });
+        // const imageUrl = imageUpload.secure_url;
+
+        const doctorData = {
+
+            name, email,
+            // image: imageUrl,
+            password: hashedPassword, speciality, degree, experience, about, fees, address: JSON.parse(address), date: Date.now()
+        }
+
+        const newDoctor = new doctorModel(doctorData);
+        await newDoctor.save();
+
+        res.json({ success: true, message: "Doctor Added To Database Successfully!" })
+
 
     } catch (error) {
-
+        console.log(error);
+        res.json({ success: false, message: error.message })
     }
 }
 
--- backend/routes_API_ENDPOINTS/adminRoute.js
+++ backend/routes_API_ENDPOINTS/adminRoute.js
@@ -1,10 +1,10 @@
 import express from "express";
-import upload from "../middlewares/multer";
-import { addDoctor } from "../controllers/adminController";
+import upload from "../middlewares/multer.js";
+import { addDoctor } from "../controllers/adminController.js";
 
 const adminRouter = express.Router();
 
 // This request is coming from /api/admin. Here the /api/admin as well as the below /add-doctor -> these both are called api enpoints
-adminRouter.post('/add-doctor', upload.single('image'), addDoctor)
+adminRouter.post('/add-doctor', upload.single("image"), addDoctor)
 
 export default adminRouter
