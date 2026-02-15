3ec244a6371631d8e3b0e053d1ba212fe85d23c3
code-biz <harisg8558@gmail.com>
2026-02-16 00:53:11 +0500

    Created the form for Add Doctor UI

--- admin/src/pages/Admin/AddDoctor.jsx
+++ admin/src/pages/Admin/AddDoctor.jsx
@@ -8,9 +8,12 @@ const AddDoctor = () => {
 
       <div className="bg-white px-8 py-8 border rounded w-full max-w-4xl max-h-[80vh] overflow-y-scroll ">
         {/*1. Div For Profile Photos */}
-        <div className="flex items-center mt-4 gap-2">
+        <div className="flex items-center mb-8 gap-4 text-gray-500">
           <label htmlFor="doc-img">
-            <img src={assets.upload_area} />
+            <img
+              src={assets.upload_area}
+              className="w-16 bg-gray-100 rounded-full cursor-pointer"
+            />
           </label>
           <input type="file" id="doc-img" hidden />
           <p>
@@ -19,24 +22,39 @@ const AddDoctor = () => {
         </div>
 
         {/*2. Div For Form */}
-        <div className="flex">
+        <div className="flex flex-col lg:flex-row items-start gap-10 text-gray-600">
           {/*2.a. Div For Left Side of Form*/}
-          <div>
-            <div>
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Name</p>
-              <input type="text" placeholder="Name" required></input>
+              <input
+                type="text"
+                placeholder="Name"
+                required
+                className="border rounded px-3 py-2"
+              ></input>
             </div>
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Email</p>
-              <input type="email" placeholder="Email" required></input>
+              <input
+                type="email"
+                placeholder="Email"
+                required
+                className="border rounded px-3 py-2"
+              ></input>
             </div>
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Doctor Password</p>
-              <input type="password" placeholder="Password" required></input>
+              <input
+                type="password"
+                placeholder="Password"
+                required
+                className="border rounded px-3 py-2"
+              ></input>
             </div>
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Expereince</p>
-              <select name="" id="">
+              <select name="" id="" className="border rounded px-3 py-2">
                 <option value="1 Year">1 Year</option>
                 <option value="2 Year">2 Year</option>
                 <option value="3 Year">3 Year</option>
@@ -50,17 +68,22 @@ const AddDoctor = () => {
                 <option value="1 Year">1 Year</option>
               </select>
             </div>
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Fees</p>
-              <input type="number" placeholder="fees" required></input>
+              <input
+                type="number"
+                placeholder="fees"
+                required
+                className="border rounded px-3 py-2"
+              ></input>
             </div>
           </div>
 
           {/*2.b. Div For Right Side of Form */}
-          <div>
-            <div>
+          <div className="w-full lg:flex-1 flex flex-col gap-4">
+            <div className="flex-1 flex flex-col gap-1">
               <p>Speciality</p>
-              <select name="" id="">
+              <select name="" id="" className="border rounded px-3 py-2">
                 <option value="General Physician">General Physician</option>
                 <option value="Gynecologist">Gynecologist</option>
                 <option value="Dermatologist">Dermatologist</option>
@@ -70,31 +93,48 @@ const AddDoctor = () => {
               </select>
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Education</p>
-              <input type="text" placeholder="Education" required></input>
+              <input
+                type="text"
+                placeholder="Education"
+                required
+                className="border rounded px-3 py-2"
+              ></input>
             </div>
 
-            <div>
+            <div className="flex-1 flex flex-col gap-1">
               <p>Address</p>
-              <input type="text" placeholder="Address 1" required></input>
-              <input type="text" placeholder="Address 2" required></input>
+              <input
+                type="text"
+                placeholder="Address 1"
+                required
+                className="border rounded px-3 py-2"
+              ></input>
+              <input
+                type="text"
+                placeholder="Address 2"
+                required
+                className="border rounded px-3 py-2"
+              ></input>
             </div>
           </div>
         </div>
 
         {/*3. Div For About Doctor */}
         <div>
-          <p>About Doctor</p>
+          <p className="mt-4 mb-2"> About Doctor</p>
           <textarea
             type="text"
             placeholder="Write about doctor"
-            className="border rounded-sm"
+            className="border  w-full px-4 pt-2 rounded"
             rows={5}
           />
         </div>
 
-        <button>Add Doctor</button>
+        <button className="text-white bg-primary mt-4 px-10 py-3 rounded-full">
+          Add Doctor
+        </button>
       </div>
     </form>
   );
