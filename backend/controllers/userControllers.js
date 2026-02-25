75991a68957d32ea939ce88350712f3e473e875d
code-biz <harisg8558@gmail.com>
2026-02-26 01:07:13 +0500

    created backend for getting and updating user profile data in DB via user route, multer, authUser, and two controllers and the cloudinary

--- backend/controllers/userControllers.js
+++ backend/controllers/userControllers.js
@@ -2,6 +2,7 @@ import validator from 'validator'
 import bcrypt from 'bcrypt'
 import userModel from '../models/userModel.js'
 import jwt from 'jsonwebtoken'
+import { v2 as cloudinary } from 'cloudinary';
 
 
 //  ********************************************************************
@@ -86,18 +87,59 @@ const loginUser = async (req, res) => {
 
 
 //  ********************************************************************
-//             API TO GET USER PROFILE DATA
+//             API TO GET USER PROFILE DATA TO SHOW ON PROFILE
 //  ____________________________________________________________________
 
 const getProfile = async (req, res) => {
     try {
 
-        const { userId } = reqbody;
+
+        const { userId } = req.body;
+        const userData = await userModel.findById(userId).select('-password');
+
+        res.json({ success: true, userData })
 
     } catch (error) {
 
+        console.log(error);
+        res.json({ success: false, message: error.message })
     }
 };
 
+//  ********************************************************************
+//             API TO UPDATE USER PROFILE DATA FROM INTERFACE
+//  ____________________________________________________________________
 
-export { registerUser, loginUser }
+const updateProfile = async (req, res) => {
+    try {
+
+
+        const { userId, name, phone, address, dob, gender } = req.body;
+        const imageFile = req.file;
+
+        if (!name || !phone || !dob || !gender) {
+            return res.json({ success: false, message: "User Data Missing!" })
+        }
+        await userModel.findByIdAndUpdate(userId, { name, phone, address: JSON.parse(address), dob, gender })
+
+        //Upload image to cloudinary and get its url that would be stored in db and shown on frontend
+        if (imageFile) {
+            const imageUpload = await cloudinary.uploader.upload(imageFile.path, { resource_type: 'image' });
+            const imageUrl = imageUpload.secure_url;
+
+            await userModel.findByIdAndUpdate(userId, { image: imageUrl });
+
+        }
+
+        res.json({ success: true, message: "Profile Updated!" })
+
+    } catch (error) {
+
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+
+};
+
+
+export { registerUser, loginUser, getProfile, updateProfile }
---
+++ backend/middlewares/authUser.js
@@ -0,0 +1,32 @@
+import jwt from 'jsonwebtoken'
+
+// Authenticate user Middleware
+// Rememebr the userToken received in the headers is processed and stored as usertoken in headers and not like userToken
+const authUser = async (req, res, next) => {
+
+    try {
+
+        const { usertoken } = req.headers;
+        console.log(usertoken);
+
+
+        if (!usertoken) {
+            return res.json({ success: false, message: "Not authorized, login again!" })
+        }
+        const userToken_decode = jwt.verify(usertoken, process.env.JWT_SECRET)
+
+        //if no body in req then create one empty body here
+        req.body = req.body || {};
+        req.body.userId = userToken_decode.id;
+        next();
+
+
+    } catch (error) {
+
+        console.log(error);
+        res.json({ success: false, message: error.message })
+    }
+
+};
+
+export default authUser
