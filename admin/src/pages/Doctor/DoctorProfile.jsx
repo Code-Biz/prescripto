3341202e0ea6220cbc046c8ae19b4d2c113b6df7
code-biz <harisg8558@gmail.com>
2026-03-07 23:59:25 +0500

    create docotr profile page and linked to backend

--- admin/src/pages/Doctor/DoctorProfile.jsx
+++ admin/src/pages/Doctor/DoctorProfile.jsx
@@ -3,19 +3,101 @@ import { DoctorContext } from "../../context/DoctorContext";
 import { useEffect } from "react";
 import { useContext } from "react";
 import { AppContext } from "../../context/AppContext";
+import { useState } from "react";
 
 const DoctorProfile = () => {
   const { dToken, profileData, setProfileData, getProfileData } =
     useContext(DoctorContext);
 
   const { currency, backendUrl } = useContext(AppContext);
+  const [isEdit, setIsEdit] = useState(false);
 
   useEffect(() => {
     if (dToken) {
       getProfileData();
     }
   }, [dToken]);
-  return profileData && <div>DoctorProfile</div>;
+  return (
+    profileData && (
+      <div>
+        <div className="flex flex-col gap-4 m-5">
+          <div>
+            <img
+              src={profileData.image}
+              className="bg-primary/80 w-full sm:max-w-64 rounded-lg"
+            />
+          </div>
+
+          <div className="flex-1 border border-stone-100 rounded-lg p-8 py-7 bg-white">
+            {/* DOC INFO : name, degree, experience */}
+            <p className="flex items-center gap-2 text-3xl font-medium text-gray-700">
+              {profileData.name}
+            </p>
+            <div className="flex items-center gap-2 mt-1 text-gray-600">
+              <p>
+                {profileData.degree}- {profileData.speciality}
+              </p>
+              <button className="py-0.5 px-2 border text-xs rounded-full">
+                {profileData.experience}
+              </button>
+            </div>
+
+            {/* DOC ABOUT */}
+            <div>
+              <p className="flex items-center gap-2 text-sm font-medium text-neutral-800 mt-3">
+                About:
+              </p>
+              <p className="text-sm text-gray-600 max-w-175 mt-1">
+                {profileData.about}
+              </p>
+            </div>
+
+            <p className="font-medium text-gray-600 mt-4">
+              Appointment Fee:{" "}
+              <span className="text-gray-800">
+                {currency}
+                {isEdit ? (
+                  <input
+                    type="number"
+                    onChange={(e) =>
+                      setProfileData((prev) => ({
+                        ...prev,
+                        fees: e.target.value,
+                      }))
+                    }
+                    value={profileData.fees}
+                  />
+                ) : (
+                  profileData.fees
+                )}
+              </span>
+            </p>
+            <div className="flex gap-2 py-2">
+              <p>Address: </p>
+              <p className="text-sm mt-1">
+                {profileData.address.line1}
+                <br />
+                {profileData.address.line2}
+              </p>
+            </div>
+            <div className="flex gap-2 pt-2">
+              <input checked={profileData.available} type="checkbox" />
+              <label for="">Available</label>
+            </div>
+            <button
+              onClick={() => {
+                setIsEdit(!isEdit);
+                console.log(isEdit);
+              }}
+              className="px-4 py-1 border border-primary text-sm rounded-full mt-5 hover:bg-primary hover:text-white transition-all"
+            >
+              Edit
+            </button>
+          </div>
+        </div>
+      </div>
+    )
+  );
 };
 
 export default DoctorProfile;
